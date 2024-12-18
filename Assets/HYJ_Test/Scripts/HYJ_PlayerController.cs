745923c5f62d15edc99f0f45596159f049b0b84e
realhyj <9k9k9k9k@naver.com>
2024-12-18 13:47:13 +0900

    Feat : 게임 일시정지 & 배속 기능 제작

--- Assets/HYJ_Test/Scripts/HYJ_PlayerController.cs
+++ Assets/HYJ_Test/Scripts/HYJ_PlayerController.cs
@@ -12,39 +12,7 @@ public class HYJ_PlayerController : MonoBehaviour
     public List<AnimationClip> Idle_List = new();
     public List<AnimationClip> Attack_List = new();
 
-    public void AnimatorSetting()
-    {
-        Animator animator = playerAnimator;
-        OverrideController = new AnimatorOverrideController();
-        OverrideController.runtimeAnimatorController = animator.runtimeAnimatorController;
-
-        // 모든 애니메이션 클립 가져오기
-        AnimationClip[] clips = animator.runtimeAnimatorController.animationClips;
-
-        foreach (AnimationClip clip in clips)
-        {
-            // 복제된 클립으로 오버라이드
-            OverrideController[clip.name] = clip;
-        }
-
-        animator.runtimeAnimatorController = OverrideController;
-        foreach (PlayerState state in Enum.GetValues(typeof(PlayerState)))
-        {
-            var stateText = state.ToString();
-            StateAnimationPairs[stateText] = new List<AnimationClip>();
-            switch (stateText)
-            {
-                case "IDLE":
-                    StateAnimationPairs[stateText] = Idle_List;
-                    break;
-                
-                case "ATTACK":
-                    StateAnimationPairs[stateText] = Attack_List;
-                    break;
-               
-            }
-        }
-    }
+    
 
     private void Update()
     {
