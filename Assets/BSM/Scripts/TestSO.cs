0079df78075cb50965b1bd719dc796146c9068d1
Vivid <suook15@naver.com>
2024-12-18 18:54:00 +0900

    [Create] 캐릭터 정보 씬 생성

---
+++ Assets/BSM/Scripts/TestSO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Test/TSO")]
+public class TestSO : ScriptableObject
+{
+    public int hp;
+    public int atk;
+    public string name;
+    public Sprite sprite;
+}
