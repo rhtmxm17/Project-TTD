f897be69c983df3ac323ae2ef43c5499339be6ae
HawoonShin <sinhaoon@gmail.com>
2024-12-19 14:31:11 +0900

    [Feat] 숙소에서 캐릭터 상호작용시 대화

--- Assets/_WorkSpace/SHW/Scripts/CharacterInteract.cs
+++ Assets/_WorkSpace/SHW/Scripts/CharacterInteract.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharacterInteract : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject talkBox;
+    // 당장 안써서 주석처리
+    // [SerializeField] TMP_Text talkText;
+
+    // 최소한의 구현...
+    // TODO : 추가적인 기능 생기면 그때 리펙토링이랑 같이 작업...하자...
+    public void ClickCharacter()
     {
-        
+        talkBox.SetActive(true);
+        StartCoroutine(OffTalkBoxCO());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator OffTalkBoxCO()
     {
-        
+        yield return new WaitForSeconds(5f);
+        talkBox.SetActive(false);
     }
 }
