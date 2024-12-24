9857c36135f2ca367164fb7307c844723d463b99
YoungsonMan <choi13570@naver.com>
2024-12-24 18:59:44 +0900

    [Fix]: 하이어아키창 배열 수정
    Right Side Panel이 가운데에 상점위에 나와서 상점 아이템 안눌러져서
    구조조정

--- Assets/_WorkSpace/YSC/01Scripts/ShopPanel.cs
+++ Assets/_WorkSpace/YSC/01Scripts/ShopPanel.cs
@@ -13,11 +13,11 @@ public class ShopPanel : BaseUI
 
     [SerializeField] TMP_Text shopNameText;
 
-    // [SerializeField] Button itemInfoSpot;
+    // [SerializeField] Button popupSpot;
     private void Start()
     {
         Init();
-        // itemInfoSpot = GetComponentInChildren<Button>();
+        // popupSpot = GetComponentInChildren<Button>();
     }
 
     private void Init()
@@ -28,11 +28,10 @@ public class ShopPanel : BaseUI
         GetUI<Button>("ShopTestButton1").onClick.AddListener(() => Open("ItemListScrollView"));
         GetUI<Button>("ShopTestButton2").onClick.AddListener(() => Open("ItemListScrollView2"));
 
+        SetPopupButtons();
 
-        // 아이템별로 버튼 다 있으니까 Item01~끝번호까지 해서 
-        // 거기서 팝업창 띄우고  팝업창이 그 아이템에 맞게 정보를 띄우기
-        GetUI<Button>("Itme01").onClick.AddListener(() => Open("ShopPopup"));
-        
+
+
     }
 
     private void SetShopPopup()
@@ -44,8 +43,24 @@ public class ShopPanel : BaseUI
         GetUI<Button>("ShopPopupClose").onClick.AddListener(() => GoBack("ShopPopup")); // 닫기버튼
         GetUI<TMP_Text>("ShopPopupText").text = "description";
         GetUI<Image>("ShopPopupImage"); // 누른 이미지에 따른 이미지 변경되도록...
-
     }
+
+
+    // TODO:
+    // 아이템별로 버튼 다 있으니까 Item01~끝번호까지 해서 
+    // 거기서 팝업창 띄우고  팝업창이 그 아이템에 맞게 정보를 띄우기
+    private void SetPopupButtons()
+    {
+        GetUI<Button>("Itme01").onClick.AddListener(() => Open("ShopPopup"));
+        GetUI<Button>("Itme02").onClick.AddListener(() => Open("ShopPopup"));
+        GetUI<Button>("Itme03").onClick.AddListener(() => Open("ShopPopup"));
+        GetUI<Button>("Itme04").onClick.AddListener(() => Open("ShopPopup"));
+        GetUI<Button>("Itme05").onClick.AddListener(() => Open("ShopPopup"));
+        GetUI<Button>("Itme06").onClick.AddListener(() => Open("ShopPopup"));
+        GetUI<Button>("Itme07").onClick.AddListener(() => Open("ShopPopup"));
+        GetUI<Button>("Itme08").onClick.AddListener(() => Open("ShopPopup"));
+    }
+
 
     private void SetStore()
     {
