53a4a10fee237ccaacee0805151c6d28f6dc2a22
JeeSeong <rhtmxm17@gmail.com>
2024-12-26 17:08:33 +0900

    [Feat]: 씬 전환 틀 구성

--- Assets/_WorkSpace/Scripts/GameManager.cs
+++ Assets/_WorkSpace/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class GameManager : SingletonBehaviour<GameManager>
 {
@@ -29,4 +30,38 @@ public class GameManager : SingletonBehaviour<GameManager>
         DontDestroyOnLoad(this);
         Input = GetComponent<PlayerInput>();
     }
+
+    #region LoadScene
+
+    /// <summary>
+    /// 스테이지 로드 시점에서만 사용, 불러올 스테이지 데이터
+    /// </summary>
+    private StageData stageData;
+
+    public void LoadMainScene()
+    {
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
+
+    public void LoadStageScene(StageData StageData)
+    {
+        this.stageData = StageData;
+        SceneManager.sceneLoaded += OnStageSceneLoaded;
+        SceneManager.LoadSceneAsync("SecondSkillSystem_kmt");
+    }
+
+    /// <summary>
+    /// 스테이지 씬 로드 완료시 스테이지 관리자 클래스 초기화(선택된 스테이지 데이터 전달)
+    /// </summary>
+    private void OnStageSceneLoaded(Scene unused1, LoadSceneMode unused2)
+    {
+        GameObject.FindWithTag("GameController").TryGetComponent(out StageManager stageManager);
+
+        // TODO: 스테이지 데이터로 초기화
+        stageManager.Initialize(stageData);
+
+        SceneManager.sceneLoaded -= OnStageSceneLoaded;
+    }
+
+    #endregion LoadScene
 }
