074aa7fd34056ce8b882a60f8c8d540edebd333f
HawoonShin <sinhaoon@gmail.com>
2024-12-26 18:06:47 +0900

    [feat] 방 꾸미기 설명 추가

--- Assets/_WorkSpace/SHW/Scripts/RoomChangePUUI.cs
+++ Assets/_WorkSpace/SHW/Scripts/RoomChangePUUI.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem.HID;
 using UnityEngine.UI;
@@ -8,8 +9,9 @@ using UnityEngine.UI;
 public class RoomChangePUUI : BaseUI
 {
     // TODO: 나중에 스크립터 오브젝트로 만들어서 테마의 이름과 내용을 가져오도록 수정 필요
-    // 현재는 임시적으로 이미지스프라이트로 
     [SerializeField] Sprite[] sprites;
+    [SerializeField] string[] names;
+    [SerializeField] string[] descriptions;
     
     int spriteIndex = 0;
 
@@ -24,21 +26,26 @@ public class RoomChangePUUI : BaseUI
         GetUI<Button>("NextButton").onClick.AddListener(()=>NextImage());
         // 이전으로 넘기는 버튼 (좌버튼)
         GetUI<Button>("BeforeButton").onClick.AddListener(()=>BeforeImage());
-        // 이미지 결정 버튼
-        // GetUI<Button>("SetImageButton").onClick.AddListener(()=>SetImage("RoomPreview"));
+        
+        // 초기 이미지 및 설명 표기용
+        GetUI<Image>("RoomPreview").sprite = sprites[spriteIndex];
+        GetUI<TMP_Text>("RoomNameText").text = names[spriteIndex];
+        GetUI<TMP_Text>("ExplainText").text = descriptions[spriteIndex];
     }
     
     // 우버튼 
     private void NextImage()
     {
-        // 능력부족의 내가 시르다..
         spriteIndex++;
         if (spriteIndex >= sprites.Length)
         {
             spriteIndex = 0;
         }
         GetUI<Image>("RoomPreview").sprite = sprites[spriteIndex];
+        GetUI<TMP_Text>("RoomNameText").text = names[spriteIndex];
+        GetUI<TMP_Text>("ExplainText").text = descriptions[spriteIndex];
     }
+    
     // 좌버튼
     private void BeforeImage()
     {
@@ -48,11 +55,7 @@ public class RoomChangePUUI : BaseUI
             spriteIndex = sprites.Length-1;
         }
         GetUI<Image>("RoomPreview").sprite = sprites[spriteIndex];
-    }
-    // 설정 버튼
-    private void SetImage(string _name)
-    {
-        // 자식으로만 되던가...
-        GetUI<Image>("BackImage").sprite = GetUI<Image>(_name).sprite;
+        GetUI<TMP_Text>("RoomNameText").text = names[spriteIndex];
+        GetUI<TMP_Text>("ExplainText").text = descriptions[spriteIndex];
     }
 }
