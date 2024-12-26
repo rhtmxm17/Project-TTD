a5660b4a018142d9fc75bfe79fd404de034321af
김민태 <66211881+don72-s@users.noreply.github.com>
2024-12-26 12:26:02 +0900

    [feat] : save charcters batch
    캐릭터 배치 저장

--- Assets/_WorkSpace/HYJ_Test/Scripts/HYJ_SelectManager.cs
+++ Assets/_WorkSpace/HYJ_Test/Scripts/HYJ_SelectManager.cs
@@ -1,7 +1,10 @@
+using Firebase.Database;
+using Firebase.Extensions;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem.LowLevel;
+using static UserDataManager;
 
 public class HYJ_SelectManager : MonoBehaviour
 {
@@ -11,13 +14,45 @@ public class HYJ_SelectManager : MonoBehaviour
     // 키 값은 위치 / 밸류 값은 유닛 고유번호;
     public Dictionary<int, int> battleInfo = new Dictionary<int, int>();
 
+    private void Awake()
+    {
+        StartCoroutine(UserDataManager.InitDummyUser(7));
+    }
+
+
     public void LookLog()
     {
-        Debug.Log("-------");
-        foreach(var (key, value) in battleInfo)
-        {
-            Debug.Log($"{key} : {value}");
-        }
-        Debug.Log("-------");
+        DatabaseReference baseref = BackendManager.CurrentUserDataRef;
+
+        baseref.Child("testDatas").SetValueAsync(null).ContinueWithOnMainThread(task => {
+
+            if (task.IsFaulted || task.IsCanceled)
+            {
+                Debug.Log("편성 초기화 실패");
+                return;
+            }
+
+            Dictionary<string, object> updates = new Dictionary<string, object>();
+
+            Debug.Log("-------");
+            foreach (var (key, value) in battleInfo)
+            {
+                updates[$"testDatas/{ key.ToString()}"] = value;
+                Debug.Log($"{key} : {value}");
+            }
+            Debug.Log("-------");
+
+            baseref.UpdateChildrenAsync(updates).ContinueWithOnMainThread(task => {
+
+                if (task.IsFaulted || task.IsCanceled)
+                {
+                    Debug.Log("추가 실패");
+                    return;
+                }
+
+            });
+
+        });
+
     }
 }
