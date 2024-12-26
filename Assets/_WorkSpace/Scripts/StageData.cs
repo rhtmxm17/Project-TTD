b87d0bc0187956e22a1b8d9fbc0a8fcd21113b09
JeeSeong <rhtmxm17@gmail.com>
2024-12-26 21:15:57 +0900

    [Feat]: 스테이지 데이터 파싱

--- Assets/_WorkSpace/Scripts/StageData.cs
+++ Assets/_WorkSpace/Scripts/StageData.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class StageData : ScriptableObject
 {
     /// <summary>
     /// 각 스테이지의 고유 번호
     /// </summary>
     public int Id => id;
+
+    /// <summary>
+    /// 스테이지를 대표하는 이름
+    /// </summary>
+    public string StageName => stageName;
 
     /// <summary>
     /// 스테이지의 적 구성
@@ -25,6 +34,8 @@ public class StageData : ScriptableObject
     public UserDataInt ClearCount { get; private set; }
 
     [SerializeField] int id;
+
+    [SerializeField] string stageName;
 
     // 우선 웨이브는 생각하지 않고 만듬
     [SerializeField] List<MonsterInfo> monsters;
@@ -70,4 +81,104 @@ public class StageData : ScriptableObject
         /// </summary>
         public int number;
     }
+
+#if UNITY_EDITOR
+    private enum Column
+    {
+        ID,
+        /// <summary>
+        /// 사용되지 않음(DataManager에서 사용)
+        /// </summary>
+        FILE_NAME,
+        STAGE_NAME,
+
+        MONSTER_NAME,
+        MONSTER_LEVEL,
+        MONSTER_POS_X,
+        MONSTER_POS_Y,
+
+        REWARD_NAME,
+        REWARD_NUMBER,
+
+    }
+
+    public void ParseCsvMultiRow(string[] lines, ref int line)
+    {
+        bool isFirst = true;
+        while (lines.Length > line)
+        {
+            string[] cells = lines[line].Split(',');
+
+            if (isFirst) // 첫 줄 한정
+            {
+                isFirst = false;
+                // ID
+                if (false == int.TryParse(cells[(int)Column.ID], out this.id))
+                {
+                    Debug.LogError($"잘못된 데이터로 갱신 시도됨");
+                    return;
+                }
+
+                monsters = new List<MonsterInfo>();
+                reward = new List<RewardInfo>();
+
+                // NAME
+                stageName = cells[(int)Column.STAGE_NAME];
+            }
+            else
+            {
+                // 첫 행에 다른 ID가 나타났다면 더이상 자신의 데이터가 아님
+                if (false == string.IsNullOrEmpty(cells[(int)Column.ID]))
+                {
+                    Debug.Log("다음 데이터에 도달");
+                    line--;
+                    return;
+                }
+            }
+
+            // 현재 행에 몬스터 정보가 있다면 추가
+            CharacterData monsterCharacterData = AssetDatabase.LoadAssetAtPath<CharacterData>($"{DataTableManager.CharacterAssetFolder}/{cells[(int)Column.MONSTER_NAME]}.asset");
+            if (monsterCharacterData != null)
+            {
+                MonsterInfo monsterInfo = new MonsterInfo() { character = monsterCharacterData };
+
+                if (false == int.TryParse(cells[(int)Column.MONSTER_LEVEL], out monsterInfo.level))
+                {
+                    Debug.LogError($"잘못된 데이터로 갱신 시도됨");
+                    continue;
+                }
+
+                if (false == float.TryParse(cells[(int)Column.MONSTER_POS_X], out monsterInfo.pose.x))
+                {
+                    Debug.LogError($"잘못된 데이터로 갱신 시도됨");
+                    continue;
+                }
+
+                if (false == float.TryParse(cells[(int)Column.MONSTER_POS_Y], out monsterInfo.pose.y))
+                {
+                    Debug.LogError($"잘못된 데이터로 갱신 시도됨");
+                    continue;
+                }
+                monsters.Add(monsterInfo);
+            }
+
+            // 현재 행에 보상 정보가 있다면 추가
+            ItemData rewardItemData = AssetDatabase.LoadAssetAtPath<ItemData>($"{DataTableManager.ItemAssetFolder}/{cells[(int)Column.REWARD_NAME]}.asset");
+            if (rewardItemData != null)
+            {
+                RewardInfo rewardInfo = new RewardInfo() { rewardItem = rewardItemData };
+
+                if (false == int.TryParse(cells[(int)Column.REWARD_NUMBER], out rewardInfo.number))
+                {
+                    Debug.LogError($"잘못된 데이터로 갱신 시도됨");
+                    continue;
+                }
+
+                reward.Add(rewardInfo);
+            }
+
+            line++;
+        }
+    }
+#endif
 }
