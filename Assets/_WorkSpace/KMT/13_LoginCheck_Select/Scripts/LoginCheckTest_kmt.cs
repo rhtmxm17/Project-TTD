576984799bbd36c5b8ec77760651960be8e755dc
김민태 <66211881+don72-s@users.noreply.github.com>
2024-12-30 13:59:20 +0900

    [feat] : login test scene
    임시로 접속화면 제작

---
+++ Assets/_WorkSpace/KMT/13_LoginCheck_Select/Scripts/LoginCheckTest_kmt.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoginCheckTest_kmt : MonoBehaviour
+{
+
+    [SerializeField]
+    Button[] buttons;
+
+
+    public void OnClick(int dummyIdx)
+    {
+        foreach (var button in buttons)
+        {
+            button.interactable = false;
+        }
+
+        Debug.Log($"로그인 시작... {dummyIdx}번...");
+
+        StartCoroutine(StartLogin(dummyIdx));
+    }
+
+    IEnumerator StartLogin(int dummyIdx)
+    {
+        yield return UserDataManager.InitDummyUser(dummyIdx);
+
+        Debug.Log("로그인 완료...");
+
+        yield return new WaitWhile(() => GameManager.UserData.Profile.Name.Value.Equals("이름 없음"));
+
+
+        UserData.myUid = $"Dummy{dummyIdx}";
+        UserData.myNickname = GameManager.UserData.Profile.Name.Value;
+
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
