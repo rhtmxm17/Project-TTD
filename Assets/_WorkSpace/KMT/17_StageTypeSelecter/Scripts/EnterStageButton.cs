f68de1fa510d13bf7088692ef7c49011b4013e47
김민태 <66211881+don72-s@users.noreply.github.com>
2025-01-02 14:23:41 +0900

    [fix] : enter boss condition
    보스 입장시 티켓을 요구하던 현상 수정

--- Assets/_WorkSpace/KMT/17_StageTypeSelecter/Scripts/EnterStageButton.cs
+++ Assets/_WorkSpace/KMT/17_StageTypeSelecter/Scripts/EnterStageButton.cs
@@ -9,29 +9,19 @@ using static GameManager;
 public class EnterStageButton : MonoBehaviour
 {
     [SerializeField]
-    StageData stageDataSO;
+    protected StageData stageDataSO;
     [SerializeField]
-    StageType stageType;
+    protected StageType stageType;
 
-    Button button;
+    protected Button button;
 
-    private void Awake()
+    protected virtual void Awake()
     {
         button = GetComponent<Button>();
         button.onClick.AddListener(() => {
 
-            ItemData goldTicketSO = DataTableManager.Instance.GetItemData(9/*골드티켓*/);
-
-            if (goldTicketSO != null && goldTicketSO.Number.Value > 0)
-            {
-                GameManager.Instance.SetLoadStageType(stageDataSO, stageType);
-                SceneManager.LoadSceneAsync("HYJ_BattleFormation");
-            }
-            else
-            {
-                Debug.Log("골드 티켓이 부족합니다.");
-            }
-
+            GameManager.Instance.SetLoadStageType(stageDataSO, stageType);
+            SceneManager.LoadSceneAsync("HYJ_BattleFormation");
 
         });
     }
