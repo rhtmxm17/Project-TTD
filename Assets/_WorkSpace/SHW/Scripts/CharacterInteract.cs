c9ef7c8ca8bb7f5bcc80b57a8813b440c760b512
HawoonShin <sinhaoon@gmail.com>
2025-01-06 09:51:27 +0900

    [feat] 캐릭터 상호작용 반복시 대사 변경
    메인과 마이룸의 경우 대사 추가를 위한 변경이 적용 되었습니다

--- Assets/_WorkSpace/SHW/Scripts/CharacterInteract.cs
+++ Assets/_WorkSpace/SHW/Scripts/CharacterInteract.cs
@@ -6,25 +6,38 @@ using UnityEngine;
 public class CharacterInteract : MonoBehaviour
 {
     [SerializeField] private GameObject talkBox;
-    // 당장 안써서 주석처리
-    // [SerializeField] TMP_Text talkText;
+    // 후에는 SO로 데이터 받아서 하는 방식으로의 전환 필요
+    [SerializeField] TMP_Text talkText;                 // 출력할 대화창
+    [SerializeField] private string[] talkdialogue;     // 대사들 모음
+    [SerializeField] private int repeatNum;             // 반복 대사 스텍
     
     private Coroutine talkCoroutine;
-
-    // 최소한의 구현...
-    // TODO: 추가적인 기능 생기면 그때 리펙토링이랑 같이 작업...하자...
+    
+    // 대화 활성화
     public void ClickCharacter()
     {
         if(talkCoroutine != null) return;
+        
+        // 주어진 대사에서 랜덤하게 대사 출력
+        talkText.text = talkdialogue[Random.Range(0,talkdialogue.Length-2)];
+        // 랜덤 대사 스택이 다 쌓이면
+        if (repeatNum == 5)
+        {
+            talkText.text = talkdialogue[talkdialogue.Length-1];
+            repeatNum = 0;
+        }
         
         talkBox.SetActive(true);
         talkCoroutine = StartCoroutine(OffTalkBoxCO());
     }
 
+    // 대화 비활성화 용 코루틴
     IEnumerator OffTalkBoxCO()
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(1f);
         talkBox.SetActive(false);
+        // 랜덤 대사 츨력을 위한 스텍 증가
+        repeatNum += 1;
         talkCoroutine = null;
     }
 }
