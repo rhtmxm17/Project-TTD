cceab64d3357b37fd51b43abb5d2938d05a5636a
JeeSeong <rhtmxm17@gmail.com>
2025-01-08 17:06:31 +0900

    [Fix]: 강화 수치 반영되도록 준비된 프로퍼티 사용

--- Assets/_WorkSpace/KMT/02_SkillSystem/Scripts/StageCharacterSetter.cs
+++ Assets/_WorkSpace/KMT/02_SkillSystem/Scripts/StageCharacterSetter.cs
@@ -72,7 +72,7 @@ public class StageCharacterSetter : MonoBehaviour
             CharacterCombatable charObj = Instantiate(characterPrefab, position[pair.Key], Quaternion.identity, transform);
             characters.Add(charObj);
 
-            charObj.Initialize(group, pair.Value);
+            charObj.InitializeWithUserData(group, pair.Value);
 
             LevelupButton levelupButton = Instantiate(levelupButtonPrefab, levelupButtonPanel.transform);
             SecondSkillButton sndSkillButton = Instantiate(secondSkillButtonPrefab, secondSkillPanel.transform);
