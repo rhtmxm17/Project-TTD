8622790e3fe29582b8c28566afcf36b131b70137
JeeSeong <rhtmxm17@gmail.com>
2025-01-13 19:03:15 +0900

    [chore]: 스토리 이펙트 유지 시간 제한

--- Assets/_WorkSpace/Scripts/StoryEffect.cs
+++ Assets/_WorkSpace/Scripts/StoryEffect.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class StoryEffect : MonoBehaviour
 {
+    [SerializeField] float lifeTime = 2f;
     public RectTransform RectTransform => GetComponent<RectTransform>();
+
+    private void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
 }
