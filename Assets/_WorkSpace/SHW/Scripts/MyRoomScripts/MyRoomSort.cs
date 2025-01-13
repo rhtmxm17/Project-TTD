a058658e2b2be103e994062885518745ff3614a6
HawoonShin <sinhaoon@gmail.com>
2025-01-13 13:51:29 +0900

    [Refactor]: 마이룸 코드 리펙토링

--- Assets/_WorkSpace/SHW/Scripts/MyRoomScripts/MyRoomSort.cs
+++ Assets/_WorkSpace/SHW/Scripts/MyRoomScripts/MyRoomSort.cs
@@ -13,7 +13,6 @@ public class MyRoomSort : MonoBehaviour
     private void Start()
     {
         charactersInvite = GetComponentsInChildren<CharacterInviteUI>().ToList();
-        
     }
     
     /// <summary>
