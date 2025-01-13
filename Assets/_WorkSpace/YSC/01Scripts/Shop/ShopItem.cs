f3ec87b9b1958516296a37a3b1924c68f19bd208
YoungsonMan <choi13570@naver.com>
2025-01-13 12:30:22 +0900

    [FIx]: 캐릭터 구매 버그 픽스
    첫구매시 재확인창 안없어지고 계속나오는거 수정
    이전 시퀀스변경과정 중에 껴있던 코드있었음. 해당코드 삭제

--- Assets/_WorkSpace/YSC/01Scripts/Shop/ShopItem.cs
+++ Assets/_WorkSpace/YSC/01Scripts/Shop/ShopItem.cs
@@ -248,14 +248,11 @@ public class ShopItem : BaseUI
     {
         // 가격 계산
         ItemData itemGive = shopItemData.Price.item;
-
-        
         var dbUpdateStream = GameManager.UserData.StartUpdateStream() // DB에 갱신 요청 시작
             .AddDBValue(shopItemData.Bought, 1);  // 요청에 '구매 횟수 증가' 등록
 
         if (null != itemGive) // 무료가 아니라면
         {
-            OpenDoubleWarning();
             Debug.Log($"소지 개수:{itemGive.Number.Value}/비용:{shopItemData.Price.gain}");
             dbUpdateStream.AddDBValue(itemGive.Number, -shopItemData.Price.gain); // 요청에 '비용 지불' 등록
             // TODO: 구매 가능/불가 판별 => 불가능 팝업
