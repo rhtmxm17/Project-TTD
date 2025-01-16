91e2df16cb50ab5635927edc3f85a24b598d32da
YoungsonMan <choi13570@naver.com>
2025-01-16 10:17:11 +0900

    [Feat]: 지형배치 버프 외각선 이펙트 추가
    용진님 프리팹 TestPosBTNBuff 직렬화필드 인스펙터에 넣을수 있게 메테리얼 추가
    
    HYJ_BtnBuff 스크립트에 버프 고르는라인에 라인추가

--- Assets/_WorkSpace/HYJ_Test/Scripts/HYJ_BtnBuff.cs
+++ Assets/_WorkSpace/HYJ_Test/Scripts/HYJ_BtnBuff.cs
@@ -1,5 +1,6 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HYJ_BtnBuff : MonoBehaviour
 {
@@ -8,16 +9,22 @@ public class HYJ_BtnBuff : MonoBehaviour
     [SerializeField] private GameObject DefenseBuffImage;
     [SerializeField] private GameObject HealBuffImage;
     
+    [Header("버프효과 이펙트 메테리얼")]                           
+    [SerializeField] Material atkBuff;  // 공격버프, 외각강조:빨간 
+    [SerializeField] Material defBuff;  // 방어버프, 외각강조:파란 
     // TODO : 현재 버프에 해당하는 걸 타일 안에 생성하고 있는데, 차라리 그냥 타일안에 버프 3개를 자식으로 두고, 버프에 따라 활성화 하는 방식이 좋을 것 같음 
     public void BuffInput(StatusBuffType statusBuffType)
     {
+        var parentObj = BuffPanel.parent.gameObject;
         switch (statusBuffType)
         {
             case StatusBuffType.ATK_PERCENTAGE:
                 Instantiate(AttackBuffImage, BuffPanel);
+                parentObj.GetComponent<Image>().material = atkBuff; // 부모 이미지 컴포넌트 메테리얼변경
                 break;
             case StatusBuffType.DEF_PERCENTAGE:
                 Instantiate(DefenseBuffImage, BuffPanel);
+                parentObj.GetComponent<Image>().material = defBuff; // 부모 이미지 컴포넌트 메테리얼변경
                 break;
         }
     }
--- Assets/_WorkSpace/HYJ_Test/Scripts/HYJ_SelectManager.cs
+++ Assets/_WorkSpace/HYJ_Test/Scripts/HYJ_SelectManager.cs
@@ -25,6 +25,7 @@ public class HYJ_SelectManager : MonoBehaviour
     [SerializeField] public GameObject CantPosUI; // 선택 불가 팝업 -> 5개 유닛이 이미 다 배치 되었을 때의 팝업
     [SerializeField] GameObject UnitChangeUI; // 유닛 변경 확인 팝업 -> 변경하시겠습니까?
     
+    
     // 키 값은 위치 / 밸류 값은 유닛 고유번호;
     public Dictionary<int, int> battleInfo = new Dictionary<int, int>();
 
@@ -71,12 +72,14 @@ public class HYJ_SelectManager : MonoBehaviour
             var obj = Instantiate(batchButtonPrefab, batchWindow);
             buttonsTransformList.Add(obj.transform);
             obj.InitDataPosBtn(i, CharacterSelectPanel, CantPosUI);
-
+            
             foreach (StageData.BuffInfo checkBuff in curStageBuff)
             {
                 if (checkBuff.tileIndex == i)
                 {
                     obj.GetComponent<HYJ_BtnBuff>().BuffInput(checkBuff.type);
+                  //  SetBuffEffect(checkBuff.type);
+                    
                 }
             }
         }
@@ -180,4 +183,7 @@ public class HYJ_SelectManager : MonoBehaviour
         //battleInfo.Clear();
         Debug.Log("Reset?" + battleInfo.Count);
     }
+    
+    
+    
 }
