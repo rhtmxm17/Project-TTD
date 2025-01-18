23d5ec34f7e062a390c79bc6ffdd48cd19c47b27
YoungsonMan <choi13570@naver.com>
2025-01-18 14:33:59 +0900

    [Fix] 데일리 리셋에서 에러, 다른 분들 실행을 위해 일단 주석처리

--- Assets/_WorkSpace/KMT/19_DailyCheck/Scripts/DailyBonus.cs
+++ Assets/_WorkSpace/KMT/19_DailyCheck/Scripts/DailyBonus.cs
@@ -16,21 +16,21 @@ public class DailyBonus : MonoBehaviour
         ItemData goldTicket = DataTableManager.Instance.GetItemData(9/*골드티켓*/);
         ItemData friendTicket = DataTableManager.Instance.GetItemData(10/*친구방문 보상 수령 카운트*/);
         
-        #region 상점아이템 일일리셋
-        // 상점아이템 구매횟수 갱신될 아이템 가져오기
-        ShopItemData[] chargeToekn = new ShopItemData[11];
-        chargeToekn[0] = DataTableManager.Instance.GetShopItemData(2);
-        chargeToekn[1]= DataTableManager.Instance.GetShopItemData(3);
-        chargeToekn[3]= DataTableManager.Instance.GetShopItemData(4);
-        chargeToekn[3]= DataTableManager.Instance.GetShopItemData(5);
-        chargeToekn[4]= DataTableManager.Instance.GetShopItemData(6);
-        chargeToekn[5]= DataTableManager.Instance.GetShopItemData(7);
-        chargeToekn[6]= DataTableManager.Instance.GetShopItemData(8);
-        chargeToekn[7]= DataTableManager.Instance.GetShopItemData(9);
-        chargeToekn[8] = DataTableManager.Instance.GetShopItemData(10);
-        chargeToekn[9] = DataTableManager.Instance.GetShopItemData(11);
-        chargeToekn[10] = DataTableManager.Instance.GetShopItemData(12);
-        #endregion
+     //   #region 상점아이템 일일리셋
+     //   // 상점아이템 구매횟수 갱신될 아이템 가져오기
+     //   ShopItemData[] chargeToekn = new ShopItemData[11];
+     //   chargeToekn[0] = DataTableManager.Instance.GetShopItemData(2);
+     //   chargeToekn[1]= DataTableManager.Instance.GetShopItemData(3);
+     //   chargeToekn[3]= DataTableManager.Instance.GetShopItemData(4);
+     //   chargeToekn[3]= DataTableManager.Instance.GetShopItemData(5);
+     //   chargeToekn[4]= DataTableManager.Instance.GetShopItemData(6);
+     //   chargeToekn[5]= DataTableManager.Instance.GetShopItemData(7);
+     //   chargeToekn[6]= DataTableManager.Instance.GetShopItemData(8);
+     //   chargeToekn[7]= DataTableManager.Instance.GetShopItemData(9);
+     //   chargeToekn[8] = DataTableManager.Instance.GetShopItemData(10);
+     //   chargeToekn[9] = DataTableManager.Instance.GetShopItemData(11);
+     //   chargeToekn[10] = DataTableManager.Instance.GetShopItemData(12);
+     //   #endregion
 
         UserDataManager.UpdateDbChain updateChain = GameManager.UserData.StartUpdateStream();
 
@@ -56,10 +56,10 @@ public class DailyBonus : MonoBehaviour
         
         /// 상점아이템 구매횟수 초기화 추가
         // 구매횟수가 최대수치보다 작거나 같으면, 회숫 0으로 변경
-        for (int i = 0; i < chargeToekn.Length; i++)
-        {
-            updateChain.SetDBValue(chargeToekn[i].Bought, 0);
-        }
+    //    for (int i = 0; i < chargeToekn.Length; i++)
+    //    {
+    //        updateChain.SetDBValue(chargeToekn[i].Bought, 0);
+    //    }
 
 
         updateChain.SetDBValue<object>("friends/visitedList", null);
